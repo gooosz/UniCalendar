6e7adce5efbf35faeac499122be9d2dc4ae793a1
Len Adler <lmadler72@eclipso.de>
2023-04-16 03:31:32 +0200

    added tui rules as comments

--- src/main/java/org/example/TUI/Terminal.java
+++ src/main/java/org/example/TUI/Terminal.java
@@ -1,4 +1,33 @@
 package org.example.TUI;
 
+/*
+ * Basic functionality of TUI:
+ *
+ * - Shows a calendar of current month
+ * - current day is selected by default
+ * - move around days with arrow keys
+ * - move around months with shift arrow.right(next month), arrow.left (last month)
+ * - move around years with shift arrow.up (next year), arrow.down (last year)
+ * - key 'q' anytime to quit
+ *
+ * on 'esc/overview':
+ * - show all moduls
+ * - arrow.up, arrow.down: move between moduls
+ * - first modul selected by default
+ * - key 'm': create new modul with name
+ * - key 'e': edit current selected modul
+ * - key 'd': delete current selected modul, ask for confirmation
+ *
+ * on selected day:
+ * - key 'enter': to zoom into day and show all events on that day fullscreen
+ * - key 'esc': zoom out
+ *
+ * on day zoom:
+ * - first event is selected by default
+ * - arrow.up, arrow.down for navigating between events
+ * - key 'n': create new event (Timeframe, Name, repeat, etc.)
+ * - key 'd': delete current event
+ * - key 'esc': zoom out
+*/
 public class Terminal {
 }
